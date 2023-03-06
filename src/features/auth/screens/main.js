c2a1f69fb9bc61c64147aaff846db1795ef7f5d0
Wael Tq <waealtaqia20152729@gmail.com>
2023-03-05 21:35:12 -0800

    Add Lottie animation

--- src/features/auth/screens/main.js
+++ src/features/auth/screens/main.js
@@ -1,16 +1,27 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import {
   MainBackground,
   MainCover,
   MainContainer,
   AuthButton,
-  Title
+  Title,
+  LottieViewWrapper
 } from "../components/main.style";
 export const MainScreen = ({ navigation }) => {
   return (
     <MainBackground>
       <MainCover />
+      <LottieViewWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </LottieViewWrapper>
       <Title>Restaurent X</Title>
       <MainContainer>
         <AuthButton
