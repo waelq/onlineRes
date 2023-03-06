3328569456ff681986e470129dd4b02837d2836f
Wael Tq <waealtaqia20152729@gmail.com>
2023-03-06 14:19:16 -0800

    setup firebase functions

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require("firebase-functions");
+// then yarn in finctions path
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
